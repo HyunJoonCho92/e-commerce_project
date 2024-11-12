a4ba929796fd2900503342f0522cda83c76247bc
DESKTOP-DGMA43C\CHO <9hjcho2@gmail.com>
2024-11-12 17:43:20 +0900

    feat: customer 회원가입 구현(이메일 인증 구현)

--- user-api/src/main/java/com/zerobase/cms/user/application/SignUpApplication.java
+++ user-api/src/main/java/com/zerobase/cms/user/application/SignUpApplication.java
@@ -1,6 +1,7 @@
 package com.zerobase.cms.user.application;
 
 import com.zerobase.cms.user.client.MailgunClient;
+import com.zerobase.cms.user.client.mailgun.SendMailForm;
 import com.zerobase.cms.user.domain.SignUpForm;
 import com.zerobase.cms.user.domain.model.Customer;
 import com.zerobase.cms.user.exception.CustomException;
@@ -10,28 +11,54 @@ import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
+
 @RequiredArgsConstructor
 public class SignUpApplication {
 
     private final MailgunClient mailgunClient;
     private final SignUpCustomerService signUpCustomerService;
 
-    public void customerSignUp(SignUpForm form) {
+    public void customerVerify(String email, String code) {
+        signUpCustomerService.verifyEmail(email, code);
+    }
+
+    public String customerSignUp(SignUpForm form) {
         if(signUpCustomerService.isEmailExist(form.getEmail())) {
             throw new CustomException(ErrorCode.ALREADY_REGISTER_USER);
         }
         else {
             Customer c = signUpCustomerService.signUp(form);
+            LocalDateTime now = LocalDateTime.now();
+
+            String code = getRandomCode();
+            SendMailForm sendMailForm = SendMailForm.builder()
+                    .from("tester@tester.com")
+                    .to(form.getEmail())
+                    .subject("Verification Email!")
+                    .text(getVerificationEmailBody(form.getEmail(), form.getName(), code))
+                    .build();
+
+            mailgunClient.sendEmail(sendMailForm);
+            signUpCustomerService.changeCustomerValidateEmail(c.getId(), code);
+
+            return "회원 가입 성공";
         }
     }
 
-//    private String getRandomCode() {
-//        return RandomStringUtils.random(10, true, true);
-//    }
-//
-//    private String getVerificationEmailBody() {
-//
-//    }
+    private String getRandomCode() {
+        return RandomStringUtils.random(10, true, true);
+    }
+
+    private String getVerificationEmailBody(String email, String name, String code) {
+        StringBuilder builder = new StringBuilder();
+        return builder.append("Hello ").append(name).append("! Please Click Link for verification.\n\n")
+                .append("http://localhost:8081/customer/signup/verify?email=")
+                .append(email)
+                .append("&code=")
+                .append(code).toString();
+    }
 }
 
